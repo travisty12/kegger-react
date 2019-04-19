b0cb99525125e12945ecd3c13ded974388b96b88
Travis Toal <travisty12@gmail.com>
2019-04-19 15:04:04 -0700

    add link and info to Info.jsx page

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Info(){
   return (
     <div>
-      <p>Kegger Taproom was created in 2019</p>
-
-      <p>We showcase different beers from The Growler Guys to demonstrate site-buiding with React.</p>
+      <h1>Kegger Taproom was created in 2019</h1>
+      <h2>We showcase different beers from The Growler Guys to demonstrate site-buiding with React.</h2>
+      <h4>For more information about the project, view the README <a href="https://www.github.com/travisty12/kegger-react">here</a>, which includes contact information and the entirety of my projects.</h4>
     </div>
   );
 }
