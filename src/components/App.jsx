5fcc2e2b632067e62529111766ea4ceb1bf3c273
Travis Toal <travisty12@gmail.com>
2019-04-19 09:41:56 -0700

    add routing and elements to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
+import Header from './Header';
+import Footer from './Footer';
+import Landing from './Landing';
+import KegList from './KegList';
 
 function App(){
   return (
     <div>
-
+      <Header/>
       <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route path='/kegs' component={KegList} />
         <Route component={Error404} />
       </Switch>
-
+      <Footer/>
     </div>
   );
 }
