08f0879384bdf70a45f6c596b74bdba9d28cebfb
Travis Toal <travisty12@gmail.com>
2019-04-19 09:05:35 -0700

    add entry file/output to webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    resolve(__dirname, 'src', 'index.jsx')
+  ],
+
+  output: {
+    filename: 'app.bundle.js',
+    path: resolve(__dirname, 'build'),
+    publicPath: '/'
+  }
+};
