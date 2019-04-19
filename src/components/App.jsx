ac7999eb88a1c8432bd358669f28361554c60402
Travis Toal <travisty12@gmail.com>
2019-04-19 09:21:03 -0700

    add basic route switch to app.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
+
+function App(){
+  return (
+    <div>
+
+      <Switch>
+        <Route component={Error404} />
+      </Switch>
+
+    </div>
+  );
+}
+
+export default App;
